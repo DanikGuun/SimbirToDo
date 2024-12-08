70f4b124c07bfd40abda527e4d7b6babd41a8e2d
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-12-08 11:15:17 +0300

    TaskListController
    Добавил UIDatePicker и кнопку для его сворота/разворота

--- SimbirToDo/Controller/TasksListController.swift
+++ SimbirToDo/Controller/TasksListController.swift
@@ -8,7 +8,13 @@ class TasksListController: UIViewController, TasksPresenterDelegate {
     
     //UI
     private var mainScroll: UIScrollView!
+    private var datePicker: UIDatePicker!
     private var taskPresenterView: TasksPresenterProtocol!
+    private var dateBarButton: UIBarButtonItem!
+    
+    //Service
+    private var isScrollViewAnimating = false
+    private var isDatePickerCollapsed = false
     
     //
     //MARK: - Lifecycle
@@ -34,13 +40,15 @@ class TasksListController: UIViewController, TasksPresenterDelegate {
     //MARK: - UI
     //
     private func setupUI(){
-        setupRightBarButton()
+        setupAddBarButton()
+        setupDateBarButton()
         setupMainScroll()
+        setupDatePicker()
         setupTaskPresenter()
     }
     
     //MARK: Right Bar Button (Добавить)
-    private func setupRightBarButton(){
+    private func setupAddBarButton(){
         let button = Button()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: button)
         
@@ -65,16 +73,70 @@ class TasksListController: UIViewController, TasksPresenterDelegate {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
+    //MARK: Date Bar Button
+    private func setupDateBarButton()
+    {
+        let image = UIImage(named: "calendar.minus")
+        dateBarButton = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(dateBarButtonPressed))
+        dateBarButton.tintColor = .systemRed
+        self.navigationItem.leftBarButtonItems = [dateBarButton]
+    }
+    
+    @objc private func dateBarButtonPressed(){
+        isScrollViewAnimating = true
+        changeDatePickerState(isCollapsed: !isDatePickerCollapsed)
+        mainScroll.setContentOffset(.zero, animated: true)
+    }
     
     //MARK: MainScroll
     private func setupMainScroll(){
         mainScroll = UIScrollView()
+        mainScroll.delegate = self
         self.view.addSubview(mainScroll)
         
         mainScroll.snp.makeConstraints { maker in
             maker.leading.trailing.top.equalTo(self.view.safeAreaLayoutGuide)
             maker.bottom.equalToSuperview()
         }
+
+    }
+    
+    //MARK: Date Picker
+    private func setupDatePicker(){
+        datePicker = UIDatePicker()
+        mainScroll.addSubview(datePicker)
+        
+        datePicker.snp.makeConstraints { maker in
+            maker.top.leading.trailing.equalToSuperview()
+            maker.width.equalToSuperview()
+            maker.height.equalTo(UIScreen.main.bounds.height / 2.5)
+        }
+        
+        datePicker.datePickerMode = .date
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        }
+    }
+    
+    private func changeDatePickerState(isCollapsed: Bool){
+        isDatePickerCollapsed = isCollapsed
+        
+        let targetHeight: CGFloat = isCollapsed ? 0 : UIScreen.main.bounds.height / 2.5
+        let targetAlpha: CGFloat = isCollapsed ? 0 : 1
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: { [weak self] in
+            self?.datePicker.snp.updateConstraints { maker in
+                maker.height.equalTo(targetHeight)
+            }
+            self?.datePicker.alpha = targetAlpha
+            self?.mainScroll.layoutIfNeeded()
+        }, completion: { [weak self] _ in
+            self?.isScrollViewAnimating = false
+        })
+        
+        let imageName = isCollapsed ? "calendar" : "calendar.minus"
+        dateBarButton.image = UIImage(named: imageName)
+        dateBarButton.tintColor = isCollapsed ? .systemBlue : .systemRed
         
     }
     
@@ -85,7 +147,8 @@ class TasksListController: UIViewController, TasksPresenterDelegate {
         mainScroll.addSubview(taskPresenterView)
         
         taskPresenterView.snp.makeConstraints { maker in
-            maker.edges.equalToSuperview()
+            maker.top.equalTo(datePicker.snp.bottom)
+            maker.bottom.equalToSuperview()
             maker.width.equalToSuperview()
             maker.height.equalTo(1500)
         }
@@ -98,3 +161,29 @@ class TasksListController: UIViewController, TasksPresenterDelegate {
     
 }
 
+//
+//MARK: - UIScrollView Delegate
+//
+extension TasksListController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let offset = scrollView.contentOffset.y
+        
+        if isScrollViewAnimating == false{
+            if offset < -80{
+                changeDatePickerState(isCollapsed: false)
+            }
+            else if offset > 30 {
+                changeDatePickerState(isCollapsed: true)
+            }
+        }
+        
+        
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        isScrollViewAnimating = false
+    }
+
+}
